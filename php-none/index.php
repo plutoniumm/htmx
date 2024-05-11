bbfecf4280476f3743cdc5c3f5a811d481e108ba
plutoniumm <51614345+plutoniumm@users.noreply.github.com>
2024-05-12 01:58:40 +0530

    mother of god easy of the century

--- php-none/index.php
+++ php-none/index.php
@@ -1,23 +1,27 @@
 <?php
-// Define routes and their corresponding content
-$routes = [
-    '/' => '<h1>Hello World</h1>',
-    '/details' => '<h2>Foo Bar</h2>'
-];
+  $route = $_SERVER['REQUEST_URI'];
 
-// Function to render content into template
-function renderTemplate($content) {
-    $template = file_get_contents('template.html');
-    $output = str_replace('<body>... here</body>', '<body>' . $content . '</body>', $template);
-    echo $output;
-}
+  if ($route === '/') {
+      $content = file_get_contents('index.html');
+      echo $content;
+  } else if ($route === '/details') {
+      $data = [
+        ['PHP', 'https://bun.sh/docs', 'https://www.php.net/images/logos/php-logo.svg'],
+        ['HTMX', 'https://htmx.org/docs', 'https://htmx.org/img/htmx_logo.2.png']
+      ];
 
-// Determine the requested route
-$route = $_SERVER['REQUEST_URI'];
+      $content = '<ul>';
+      foreach ($data as $item) {
+        $content .= '<li>';
+          $content .= '<img src="' . $item[2] . '" />';
+          $content .= '<a href="' . $item[1] . '">' . $item[0] . '</a>';
+        $content .= '</li>';
+      }
+      $content .= '</ul>';
 
-// Get content for the requested route
-$content = isset($routes[$route]) ? $routes[$route] : '';
-
-// Render content into template
-renderTemplate($content);
+      echo $content;
+  } else {
+      http_response_code(404);
+      echo '404 Not Found';
+  }
 ?>
